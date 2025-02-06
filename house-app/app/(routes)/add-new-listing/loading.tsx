8c18c4d16671e474f7a0f16b2ce355a410b38216
nikakaladze <nikakaladze89@gmail.com>
2025-02-06 16:48:21 +0400

    chore:Working for routes

---
+++ house-app/app/(routes)/add-new-listing/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex w-full h-screen justify-center items-center text-3xl'> Loading...</div>
+  )
+}
+
+export default loading
--- house-app/next.config.ts
+++ house-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    domains: ['oxmjhfafozlafqrhhawl.supabase.co', 'img.clerk.com']
+  }
 };
 
 export default nextConfig;
