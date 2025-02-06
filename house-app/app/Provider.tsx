52e50d6bb6076de3e4318ea58c17ae63ef451f3e
nikakaladze <nikakaladze89@gmail.com>
2025-02-06 17:26:28 +0400

    chore:add variables in global.css

--- house-app/app/Provider.tsx
+++ house-app/app/Provider.tsx
@@ -1,18 +1,18 @@
 "use client";
 import React from "react";
 import Header from "./_components/Header";
-// import { LoadScript } from "@react-google-maps/api";
+import { LoadScript } from "@react-google-maps/api";
 
 function Provider({ children }) {
   return (
     <div>
-      {/* <LoadScript
+      <LoadScript
         googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY}
         libraries={["places"]}
-      > */}
+      >
         <Header />
         <div className="mt-[90px]">{children}</div>
-      {/* </LoadScript> */}
+      </LoadScript>
     </div>
   );
 }
