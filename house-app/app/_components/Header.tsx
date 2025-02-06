5cc3f03d50ea41be15b3269087e48828ac41dadf
nikakaladze <nikakaladze89@gmail.com>
2025-02-06 16:20:23 +0400

    chore:Add shadcn

--- house-app/app/_components/Header.tsx
+++ house-app/app/_components/Header.tsx
@@ -25,7 +25,7 @@ function Header() {
     <div className="p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white">
       <div className="flex gap-12 items-center">
         <Link href={"/"}>
-          <Image src={"/logo.svg"} width={150} height={150} alt="logo" />
+          <Image src={"/house-logo.svg"} width={100} height={100} alt="logo" />
         </Link>
         <ul className="hidden md:flex gap-10">
           <Link href={"/"}>
