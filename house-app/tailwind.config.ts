4a0f6f8beeb5ab204a41405da27cfec22673c6c7
nikakaladze <nikakaladze89@gmail.com>
2025-02-06 16:48:21 +0400

    chore:Working for routes

--- house-app/tailwind.config.ts
+++ house-app/tailwind.config.ts
@@ -21,7 +21,7 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '7f57f1',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
